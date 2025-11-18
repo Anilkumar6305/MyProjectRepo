dc5c9b1fe59675ade2959710efa9854cbd429676
Anilkumar6305 <anilkumarjalamana1999@gmail.com>
2025-11-19 00:08:54 +0530

    Added the version3 code

--- ap.php
+++ ap.php
@@ -11,3 +11,21 @@ $Lightning = new Car();
 // show object properties
 echo $Lightning->model;
 ?>
+
+//Version3 code adding
+<?php
+    // Switch Statement Example
+    switch ($i) {
+        case "free":
+            echo "i is free";
+            break;
+        case "code":
+            echo "i is code";
+            break;
+        case "camp":
+            echo "i is camp";
+            break;
+        default:
+            echo "i is freecodecamp";
+            break;
+    }
