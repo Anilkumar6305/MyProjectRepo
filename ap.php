ccdb83a995890a1da3a1e0e43d7b4399394aceaa
Anilkumar6305 <anilkumarjalamana1999@gmail.com>
2025-11-27 22:52:34 +0530

    Update ap.php

--- ap.php
+++ ap.php
@@ -25,15 +25,3 @@ echo "Age: " . $age . "<br>";
 echo "Price: " . $price . "<br>";
 echo "Is Student: " . ($isStudent ? "Yes" : "No") . "<br>";
 ?>
-//adding the version-04 code
-<?php
-$name = "Alice"; // String
-$age = 30; // Integer
-$price = 19.99; // Float
-$isStudent = true; // Boolean
-
-echo "Name: " . $name . "<br>";
-echo "Age: " . $age . "<br>";
-echo "Price: " . $price . "<br>";
-echo "Is Student: " . ($isStudent ? "Yes" : "No") . "<br>";
-?>
